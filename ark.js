46a526b63e42b421d384e9a023c702f26866a05c
AeonPSR <floriandevigne1@hotmail.fr>
2024-05-17 14:19:42 +0200

    Testing around the age stuff, going for traits now

--- ark.js
+++ ark.js
@@ -14,20 +14,15 @@ class Character {
 	}
 }
 
-function getRandomAge() {
+function getRandomAge(trait) {
 	//Age from 5 to 120
 	//Should be more commonly between 25 and 55
-	const age = getRandomInt(30)+25; // Base age between 25 and 55
-	
-	if (getRandomInt(99) > 49) //50% odd it's old
-		age = age+ //TODO
-	else { //50% it's young
-
-	} 
-
-	age = Math.max(1, finalAge); // Minimum age is 1
-	age = Math.min(125, finalAge); // Maximum age is 125
-	return age;
+	//Let's use a trait system to give age range to the population, and so the
+	//odds and repartition will be dictated by the odds of each trait appearing.
+	const minAge = 5;
+	const maxAge = 120;
+	const range = maxAge - minAge;
+	return minAge + getRandomInt(range);
 }
 
 function createCharacterList() {
@@ -43,28 +38,6 @@ function createCharacterList() {
 	return characters;
 }
 
-function calculateStatistics(iterations) {
-	const ages = [];
-	// Generate ages
-	for (let i = 0; i < iterations; i++) {
-		ages.push(getRandomAge());
-	}
-	// Calculate average
-	const sum = ages.reduce((acc, curr) => acc + curr, 0);
-	const average = sum / iterations;
-	// Calculate standard deviation
-	const variance = ages.reduce((acc, curr) => acc + Math.pow(curr - average, 2), 0) / iterations;
-	const stdDeviation = Math.sqrt(variance);
-	// Find maximum and minimum ages
-	const maxAge = Math.max(...ages);
-	const minAge = Math.min(...ages);
-	// Display results
-	console.log("Average Age:", average.toFixed(2));
-	console.log("Standard Deviation:", stdDeviation.toFixed(2));
-	console.log("Maximum Age:", maxAge);
-	console.log("Minimum Age:", minAge);
-}
-
 function displayCharacters(characters) {
 	const characterList = $('#characterList');
 	characters.forEach(character => {
@@ -74,7 +47,6 @@ function displayCharacters(characters) {
 }
 
 $(document).ready(function() {
-//	const characters = createCharacterList();
-//	displayCharacters(characters);
-	calculateStatistics(5000);
+	const characters = createCharacterList();
+	displayCharacters(characters);
 });
